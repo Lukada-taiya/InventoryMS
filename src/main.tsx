12a98953aef3621a70abbcf0d8b730088d7818ef
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-18 14:03:27 +0000

    Dependency update

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './css/style.css';
-import './css/satoshi.css';
-import 'jsvectormap/dist/css/jsvectormap.css';
+import './css/satoshi.css'; 
 import 'flatpickr/dist/flatpickr.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
