7841425b98eddb618b2d822ce2ee22c67948ebe8
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-08 18:49:59 +0000

    Feat: Implemented #12 feature

---
+++ src/images/index.ts
@@ -0,0 +1,9 @@
+import logo from './logo/logo.png';
+import building from './cover/building.png';
+import userThree from './user/user-03.png';
+
+export {
+    logo,
+    building,
+    userThree
+}
