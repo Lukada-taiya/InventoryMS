4539d67e601dea471ac9c04a0f96873918f7c65a
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-10 22:26:31 +0000

    Enhancement: Added proxy to bypass cors restrictions

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,29 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '^/api': {
+        target: "https://localhost:7000/",
+        secure: false
+      },
+      '^/login': {
+        target: "https://localhost:7000/",
+        secure: false
+      },
+      '^/pingauth': {
+        target: "https://localhost:7000/",
+        secure: false
+      },
+      '^/register': {
+        target: "https://localhost:7000/",
+        secure: false
+      },
+      '^/logout': {
+        target: "https://localhost:7000/",
+        secure: false
+      },
+    },
+    port: 5173
+  }
 })
